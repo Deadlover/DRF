aa1cedfea5fbd8686ff28ae2c9ea69551835e85b
love <kshitizluv123@gmail.com>
2024-03-18 15:37:07 +0545

    refresh token added

--- login/urls.py
+++ login/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
-    path('',views.LoginPage.as_view(),name='Loginpage'),
-    path('signup/',views.SignupPage.as_view(),name='Signup'),
+    path("", views.LoginPage.as_view(), name="Loginpage"),
+    path("signup/", views.SignupPage.as_view(), name="Signup"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
